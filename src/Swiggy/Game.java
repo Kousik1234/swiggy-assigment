4584b74408179af5669885163b928efa7de5a217
Kousik1234 <kousikmanik8945@gmail.com>
2023-06-01 13:00:52 +0530

    besic of player class

---
+++ src/Swiggy/Game.java
@@ -0,0 +1,18 @@
+package Swiggy;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Game {
+
+    private List<Player> players;
+    private Stack<Card> drawPile;
+    private Stack<Card> discardPile;
+    private int currentPlayerIndex;
+    private int direction;
+
+
+
+
+
+}
