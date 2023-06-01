bf2eaf53780ea1ab9c9f35a7e7d3cbe485ac5618
Kousik1234 <kousikmanik8945@gmail.com>
2023-06-01 13:21:58 +0530

    Game class completed

--- src/Swiggy/Game.java
+++ src/Swiggy/Game.java
@@ -26,6 +26,14 @@ public class Game {
         discardPile.push(drawPile.pop());
     }
 
+    /* method is used to set up the initial deck of cards for the game.
+    It iterates over all possible combinations of ranks and suits (using nested loops)
+     and creates a Card object for each combination. These Card objects are then added to
+     the drawPile stack, representing the deck of cards.
+
+     By shuffling the deck, each player will have an equal
+     chance of receiving any particular card during the initial dealing process.
+     */
 
     private void initializeDeck() {
 
@@ -36,6 +44,18 @@ public class Game {
         }
         Collections.shuffle(drawPile);
     }
+
+
+    /*
+    The dealCards() method is responsible for distributing the initial cards
+    to each player at the start of the game. It uses nested loops to iterate
+    over each player and deals a fixed number of cards (5 in this case) to each player.
+
+    The outer loop iterates 5 times because we want to deal 5 cards to each player.
+    The inner loop iterates over each player in the players list and assigns a card
+    to that player by calling the addCardToHand() method on the player object.
+    The card is taken from the top of the drawPile stack using the pop() method.
+    * */
 
     private void dealCards() {
         for (int i=0 ; i<5 ;i++) {
@@ -48,6 +68,64 @@ public class Game {
     private Player getCurrentPlayer() {
         return players.get(currentPlayerIndex);
     }
+    private void skipNextPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + direction) % players.size();
+    }
+    private void reverseDirection() {
+        direction *= -1;
+    }
 
+    private void drawCards(int numCards) {
+        Player currentPlayer = getCurrentPlayer();
+        for (int i = 0; i < numCards; i++) {
+            if (drawPile.isEmpty()) {
+                // Game ends in a draw
+                return;
+            }
+            currentPlayer.addCardToHand(drawPile.pop());
+        }
+    }
+
+    private void nextPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + direction + players.size()) % players.size();
+    }
+
+    private void declareWinner(Player player) {
+        System.out.println("Player " + player.getName() + " wins!");
+    }
+
+
+    public void playCard(int index) {
+        Player currentPlayer = getCurrentPlayer();
+        Card card = currentPlayer.playCard(index);
+        discardPile.push(card);
+
+        if (card.getRank() == Rank.ACE) {
+            skipNextPlayer();
+        } else if (card.getRank() == Rank.KING) {
+            reverseDirection();
+        } else if (card.getRank() == Rank.QUEEN) {
+            drawCards(2);
+        } else if (card.getRank() == Rank.JACK) {
+            drawCards(4);
+        }
+
+        if (currentPlayer.getHand().isEmpty()) {
+            declareWinner(currentPlayer);
+        } else {
+            nextPlayer();
+        }
+    }
+
+    public void printGameState() {
+        System.out.println("Current player: " + getCurrentPlayer().getName());
+        System.out.println("Top card on discard pile: " + discardPile.peek());
+        System.out.println("Draw pile size: " + drawPile.size());
+        System.out.println("Current direction: " + (direction == 1 ? "Clockwise" : "Counter-clockwise"));
+
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s hand: " + player.getHand());
+        }
+    }
 
 }
